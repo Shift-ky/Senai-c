e0b1ffd55715e8aa2abd56b0495d3eb47d2644c8
Robson Pinheiro Da Costa Paiva <103571578+Shift-ky@users.noreply.github.com>
2022-09-08 17:57:27 -0300

    atividade

---
+++ JAVA/ATIVIDADE-5/ATIVIDADE1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/*01. Escreva um algoritmo que exiba 20 vezes a mensagem “Eu amo Java!”.
+*/
+public class ATIVIDADE1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++){
+            System.out.println("EU AMO JAVA!");
+        }
+
+    }
+}
