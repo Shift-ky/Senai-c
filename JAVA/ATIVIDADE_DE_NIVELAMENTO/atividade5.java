3e76d415a6d5ed6bbcb47885a7110da51bcec0c2
Robson Pinheiro Da Costa Paiva <103571578+Shift-ky@users.noreply.github.com>
2022-09-21 16:36:36 -0300

    atualização

--- JAVA/ATIVIDADE_DE_NIVELAMENTO/atividade5.java
+++ JAVA/ATIVIDADE_DE_NIVELAMENTO/atividade5.java
@@ -12,13 +12,14 @@ public class atividade5 {
     public static double desconto(int quant){ // Inicio da segunda função
         double desconto;                    
         if(quant <= 5){
-                    desconto = 0.98;
-                }else if( quant <= 10){
-                    desconto = 0.97;
-                } else{
-                    desconto = 0.95;
+            desconto = 0.98;
+        }else if( quant <= 10){
+            desconto = 0.97;
+        } else{
+            desconto = 0.95;
                     
-                }
+        }
+
         return desconto;
     } // Fim da segunda função
     
@@ -28,7 +29,7 @@ public class atividade5 {
         int cont1 = 0, cont2 = 0,cont3 = 0,x;
         for(int i = 0; i < 20; i++){
             System.out.printf("Digite o %dª valor: ",i+1);
-                x = entrada.nextInt();
+            x = entrada.nextInt();
                     
             if( x < 100){
                 cont1++;
@@ -42,16 +43,13 @@ public class atividade5 {
         entrada.close();
         
        System.out.println("Foram digitados "+cont1+ "Numero menores que 100\n"+
-                "Foram digitados "+cont2+" numeros entre 100 e 200 \n"+
-                "Foram digitados "+cont3+" numeros acima de 200\n");
+        "Foram digitados "+cont2+" numeros entre 100 e 200 \n"+
+        "Foram digitados "+cont3+" numeros acima de 200\n");
     } // fim da terceira função
     
-    
-   
-    
     public static void soma() {   //função da quarta atividade
-    	int opc2,soma=0;
-    	Scanner entrada = new Scanner(System.in);
+        int opc2,soma=0;
+        Scanner entrada = new Scanner(System.in);
         
         do{
             System.out.printf("Digite um valor positivo, ou um negativo para sair do programa \n -> ");
@@ -72,16 +70,19 @@ public class atividade5 {
         
         System.out.println("Escolha uma atividade");
         System.out.printf("[1] - Atividade 1 \n[2] - Atividade 2 \n"
-                + "[3] - Atividade 3 \n[4] - Atividade 4 \n-> ");
+        + "[3] - Atividade 3 \n[4] - Atividade 4 \n-> ");
         opc = entrada.nextInt();
         
         switch(opc){
             case 1:
                 double custoFabrica,distribuidor = 0.28,impostos = 0.45;
-                                System.out.println("Digite o valor de fábrica do veículo: ");
+
+                System.out.println("Digite o valor de fábrica do veículo: ");
                 custoFabrica = entrada.nextDouble();
+
                 double custoCarro = somaTotal(custoFabrica, distribuidor, impostos);
                 System.out.printf("O valor total do veículo é: %f",custoCarro);
+
             break;
             
             case 2:
@@ -97,7 +98,7 @@ public class atividade5 {
                 valorUnitario = entrada.nextDouble();
                 
                 total = quantidade * valorUnitario;
-                 desconto = desconto(quantidade);
+                desconto = desconto(quantidade);
                 System.out.printf("%d unidades de %s é R$ %.2f",quantidade,nome,total*desconto);
             break;
             
